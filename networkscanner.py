83aa5098fae9183a9ff9708ab6f657378e28d4de
Sara Husain <133413387+sh-919@users.noreply.github.com>
2025-08-27 18:43:01 -0400

    Create networkscanner.py

---
+++ networkscanner.py
@@ -0,0 +1,37 @@
+import scapy.all as scapy
+import optparse
+from termcolor import colored
+
+def get_arguments():
+    parse = optparse.OptionParser()
+    parse.add_option("-t", "--target", dest = "ip", help = "An example of an IP Address range => 192.168.29.1/24.")
+    someoptions, arguments = parse.parse_args()
+
+    if not someoptions.ip:
+        parse.error("[-] Please enter an IP Address, use --help for additional info")
+    else:
+        return someoptions
+
+def scan(ip):
+    arp_request = scapy.ARP(pdst = ip)
+    broadcast = scapy.Ether(dst = "ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast/arp_request   
+    answered_list, unanswered_list = scapy.srp(arp_request_broadcast, timeout = 1, verbose = False)
+    client_list = []
+    for element in answered_list:
+        client_dict = {"ip" : element[1].psrc, "mac" : element[1].hwsrc}
+        client_list.append(client_dict)
+    return client_list
+
+def print_result(result_list):
+    print("IP Address\t\tMAC Address\n---------------------------------------")
+    for result in result_list:
+        print(result["ip"], "\t    ",result["mac"])
+
+#def about():
+#workon
+
+#about()
+someoptions = get_arguments()
+scan_output = scan(someoptions.ip)
+print_result(scan_output)
